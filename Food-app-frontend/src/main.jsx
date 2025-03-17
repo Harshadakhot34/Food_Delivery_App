ac913dcb1ba60cf406f055dd4a88f82e0c2127cd
Harshadakhot34 <hkhot30@gmail.com>
2025-03-17 17:27:55 +0530

    Added remaining code

--- Food-app-frontend/src/main.jsx
+++ Food-app-frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import StoreContextProvider from './context/StoreContext.jsx';
-console.log('App is mounting...');
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import StoreContextProvider from "./context/StoreContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <StoreContextProvider>
+    <StoreContextProvider>
       <App />
-      </StoreContextProvider>
+    </StoreContextProvider>
   </BrowserRouter>
 );
